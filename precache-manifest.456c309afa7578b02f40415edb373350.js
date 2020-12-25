cbeaf934d39874c80ad89b03868a8e2a799a22d7
Junaid Latif <junaidlatif004@gmail.com>
2020-12-25 21:27:36 +0500

    Updates

---
+++ precache-manifest.456c309afa7578b02f40415edb373350.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08387aaeed93bb72c5f28b5643c4745f",
+    "url": "/todolist-reactapp/index.html"
+  },
+  {
+    "revision": "464fbcd5a19a25170633",
+    "url": "/todolist-reactapp/static/css/main.eb090266.chunk.css"
+  },
+  {
+    "revision": "07abe8c09070efb7c7fc",
+    "url": "/todolist-reactapp/static/js/2.d2d6b657.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todolist-reactapp/static/js/2.d2d6b657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464fbcd5a19a25170633",
+    "url": "/todolist-reactapp/static/js/main.3786b47f.chunk.js"
+  },
+  {
+    "revision": "c722d8fd9979abad86cb",
+    "url": "/todolist-reactapp/static/js/runtime-main.5710f6e6.js"
+  }
+]);
